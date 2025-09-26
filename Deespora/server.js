2b296a3d1d6ac5f9c1628fcebeb2dd62667e5ae0
Mac kingsley <kingsleyudoma2018@gmail.com>
2025-09-26 09:18:01 +0100

    pushed

---
+++ Deespora/server.js
@@ -0,0 +1,30 @@
+
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv");
+
+require("./src/utils/db.js");
+const routes = require("./src/routes/routes.js");
+
+dotenv.config();
+const app = express();
+
+
+app.use(cors({ origin: process.env.CLIENT_URL || "*", credentials: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) =>
+  res.json({ ok: true, name: "firebase-phone-auth-express-template" })
+);
+
+app.use("/", routes);
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
