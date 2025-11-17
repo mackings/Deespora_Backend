9a352322d6df1a83de7274907fff57ab2da3e7c4
Mac kingsley <kingsleyudoma2018@gmail.com>
2025-11-17 08:35:58 +0100

    push

--- Deespora/server.js
+++ Deespora/server.js
@@ -19,6 +19,7 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:5173',
   'http://localhost:5174',
+  "https://deespora.netlify.app",
   process.env.CLIENT_URL
 ].filter(Boolean); // Remove undefined values
 
