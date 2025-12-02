47ff2d112e03f8cf3e0987fab3fa0cb2603dc0ce
Mac kingsley <kingsleyudoma2018@gmail.com>
2025-12-02 09:16:31 +0100

    pp

--- Deespora/src/utils/twilio.js
+++ Deespora/src/utils/twilio.js
@@ -1,6 +1,6 @@
 const twilio = require('twilio');
 
-// Use live credentials
+
 const accountSid = process.env.TWILIO_ACCOUNT_SID
 const authToken = process.env.TWILIO_AUTH_TOKEN
 const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER
